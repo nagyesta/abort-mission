28067ce0b2717152b18ebf14b21755cc68ec1048
Esta Nagy <nagyesta@gmail.com>
2022-12-03 07:43:41 +0100

    Annotation based matcher for test class evaluation - Adds dependency extractor for grabbing the simple names of class level annotations - Adds new test cases
    Resolves #198
    {minor}
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

---
+++ mission-control/src/main/java/com/github/nagyesta/abortmission/core/extractor/impl/AnnotationSimpleNameDependencyExtractor.java
@@ -0,0 +1,34 @@
+package com.github.nagyesta.abortmission.core.extractor.impl;
+
+import com.github.nagyesta.abortmission.core.extractor.DependencyNameExtractor;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class AnnotationSimpleNameDependencyExtractor implements DependencyNameExtractor {
+
+    @Override
+    public Optional<Set<String>> apply(final Object o) {
+        if (!(o instanceof Class<?>)) {
+            return Optional.empty();
+        }
+
+        final Class<?> testClass = (Class<?>) o;
+        final Set<String> dependencies = Arrays.stream(testClass.getAnnotations())
+                .map(annotation -> annotation.annotationType().getSimpleName()).collect(Collectors.toSet());
+        return wrapWithOptional(new TreeSet<>(dependencies));
+    }
+
+    private static Optional<Set<String>> wrapWithOptional(final Set<String> dependencies) {
+        final Optional<Set<String>> result;
+        if (dependencies.isEmpty()) {
+            result = Optional.empty();
+        } else {
+            result = Optional.of(dependencies);
+        }
+        return result;
+    }
+}
